59b685e3aabb713038dacaa31ee46e7fde7311fc
lyjguy <lyjguy@naver.com>
2022-06-20 13:15:57 +0900

    사용하지 않는 테스트 어노테이션 코드 제거

--- src/test/kotlin/com/lyjguy/kotlinspringgraphql/KotlinSpringGraphqlApplicationTests.kt
+++ src/test/kotlin/com/lyjguy/kotlinspringgraphql/KotlinSpringGraphqlApplicationTests.kt
@@ -1,9 +1,7 @@
 package com.lyjguy.kotlinspringgraphql
 
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
 class KotlinSpringGraphqlApplicationTests {
 
     @Test
